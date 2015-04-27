e8092103128e9fa255e821d5ec1e199efd19daa5
AJ Green <ajgreen1987@gmail.com>
2015-04-26 23:34:14 -0400

    - First commit - Added gif animation support

--- TickTime/ViewController.m
+++ TickTime/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImage+animatedGIF.h"
 
 @interface ViewController ()
 
@@ -17,11 +18,26 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self animateBackground];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void) animateBackground
+{
+    NSString *path=[[NSBundle mainBundle]pathForResource:@"gif" ofType:@"gif"];
+    NSURL *url=[[NSURL alloc] initFileURLWithPath:path];
+    self.animatedBackground.image= [UIImage animatedImageWithAnimatedGIFURL:url];
+    
+    double delayInSeconds = 10.9f;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+        self.animatedBackground.image = [UIImage imageNamed:@"lastFrame"];
+    });
 }
 
 @end
